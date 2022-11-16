c1dc2434733650e5ff7024e0230aeab888e9c987
Mitchell Bylak <mjbylak@uark.edu>
2022-11-15 22:43:13 -0600

    mitchell-metalDetector-firstDraft

--- app/src/main/java/com/example/android_swiss_army_knife/ui/metal_detector/MetalDetectorViewModel.kt
+++ app/src/main/java/com/example/android_swiss_army_knife/ui/metal_detector/MetalDetectorViewModel.kt
@@ -1,14 +1,52 @@
 package com.example.android_swiss_army_knife.ui.metal_detector
 
 import android.app.Application
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.AndroidViewModel
+import com.example.android_swiss_army_knife.SensorLiveData
 
 class MetalDetectorViewModel(application: Application) : AndroidViewModel(application) {
+    private var state: MetalDetectorViewModel.SensorState = MetalDetectorViewModel.SensorState()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is metal detector Fragment"
+    private var magneticFieldMeasurement: String = "0.0"
+
+    val text = MutableLiveData<String>().apply {
+        value = "0.0"
     }
-    val text: LiveData<String> = _text
+//    val text: LiveData<String> = text
+
+    fun updateTextWithSensorValue() {
+       text.value =" MAGNETIC FIELD: " + magneticFieldMeasurement
+    }
+
+    fun registerSensors() { // use entire block for each sensor you need in this class
+        state!!.sensorMagneticFieldLiveData = registerSpecificSensor(Sensor.TYPE_MAGNETIC_FIELD) // for each sensor
+        state!!.sensorMagneticFieldLiveData!!.observeForever { event: SensorLiveData.Event? ->
+            if (event != null) {
+                magneticFieldMeasurement = event.value.toString()
+                updateTextWithSensorValue()
+            }
+        }
+    }
+
+    fun deregisterSensors() { // set all sensors as inactive
+        state!!.sensorMagneticFieldLiveData!!.setInactive() // required for each sensor you use
+    }
+
+    private fun registerSpecificSensor(sensorType: Int): SensorLiveData { // do not change
+        return SensorLiveData(
+            getApplication<Application>().applicationContext,
+            sensorType,
+            SensorManager.SENSOR_DELAY_UI
+        )
+    }
+
+    private class SensorState { // add additional sensors here
+        var sensorAccelerometerLiveData: SensorLiveData? = null
+        var sensorMagneticFieldLiveData: SensorLiveData? = null
+
+    }
 }
