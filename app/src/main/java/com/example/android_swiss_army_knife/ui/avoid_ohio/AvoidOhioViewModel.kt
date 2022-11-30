9c5aa1b10371d3957096307b5964b866bf1a1020
jakilgo <justin.kilgo314@gmail.com>
2022-11-29 22:18:22 -0600

    added direction

--- app/src/main/java/com/example/android_swiss_army_knife/ui/avoid_ohio/AvoidOhioViewModel.kt
+++ app/src/main/java/com/example/android_swiss_army_knife/ui/avoid_ohio/AvoidOhioViewModel.kt
@@ -5,75 +5,89 @@ import android.app.Application
 import android.hardware.Sensor
 import android.hardware.SensorManager
 import android.location.Location
-import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.*
 import com.example.android_swiss_army_knife.util.*
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationServices
 
 class AvoidOhioViewModel(application: Application) : AndroidViewModel(application) {
-    private val thisContext = getApplication<Application>().applicationContext
+    private val thisContext by lazy { getApplication<Application>().applicationContext }
+    private var state: SensorState = SensorState()
 
-    //Boolean to keep track of whether permissions have been granted
+    // begin location variables
     var locationPermissionEnabled:Boolean = false
-    //Boolean to keep track of whether activity is currently requesting location Updates
     private var locationRequestsEnabled:Boolean = false
-    //Member object for the FusedLocationProvider
     private lateinit var locationProviderClient: FusedLocationProviderClient
-    //Member object for the last known location
-    private lateinit var mCurrentLocation: Location
-    //Member object to hold onto locationCallback object
-    //Needed to remove requests for location updates
     private lateinit var mLocationCallback: LocationCallback
 
-    // Launcher for locationPermissions. To be launched when location permissions
-    // are not available
     lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
 
-    //LocationUtilCallback object
-    //Dynamically defining two results from locationUtils
-    //Namely requestPermissions and locationUpdated
     private val locationUtilCallback = object:LocationUtilCallback{
-        //If locationUtil request fails because of permission issues
-        //Ask for permissions
         override fun requestPermissionCallback() {
             locationPermissionRequest.launch(arrayOf(
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION))
         }
-        //If locationUtil returns a Location object
-        //Populate the current location and log
         override fun locationUpdatedCallback(location: Location) {
-            mCurrentLocation = location
-            _text.value = "Location is [Lat: ${location.latitude}, Long: ${location.longitude}]"
+            state.sensorGpsLiveData = location
+            _text.value = "Location is [Lat: ${location.latitude}, Long: ${location.longitude}]\nDirection is: ${compassRotation.value}"
         }
     }
 
     fun startLocationRequests(){
-        //If we aren't currently getting location updates
         if(!locationRequestsEnabled){
-            //create a location callback
             mLocationCallback = createLocationCallback(locationUtilCallback)
-            //and request location updates, setting the boolean equal to whether this was successful
             locationRequestsEnabled = createLocationRequest(thisContext,locationProviderClient,mLocationCallback)
         }
     }
-
-    private var state: SensorState = SensorState()
+    // end location variables
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is avoid ohio Fragment"
+        value = ""
     }
     val text: LiveData<String> = _text
+
+    private var accelerometerMeasurement: FloatArray = FloatArray(3)
+    private var magneticFieldMeasurement: FloatArray = FloatArray(3)
+    private val mR = FloatArray(9)
+    private val mOrientation = FloatArray(3)
+
+    private val compassRotation = MutableLiveData<Double>().apply {
+        value = 0.0
+    }
+
+    fun updateTextWithSensorValues() {
+        SensorManager.getRotationMatrix(mR, null, accelerometerMeasurement, magneticFieldMeasurement);
+        SensorManager.getOrientation(mR, mOrientation);
+        var compassRadianMeasurement = mOrientation[0]
+        var compassDegreeMeasurement = (Math.toDegrees(compassRadianMeasurement.toDouble())+360)%360;
+        if (Math.abs(compassDegreeMeasurement - compassRotation.value!!) > 5)
+        {
+            compassRotation.value = compassDegreeMeasurement
+        }
+    }
 
     fun registerSensors() { // use entire block for each sensor you need in this class
         locationProviderClient = LocationServices.getFusedLocationProviderClient(thisContext)
         getLastLocation(thisContext,locationProviderClient,locationUtilCallback)
+        state!!.sensorMagneticFieldLiveData = registerSpecificSensor(Sensor.TYPE_MAGNETIC_FIELD) // for each sensor
+        state!!.sensorAccelerometerLiveData = registerSpecificSensor(Sensor.TYPE_ACCELEROMETER)
+        state!!.sensorMagneticFieldLiveData!!.observeForever { event: SensorLiveData.Event? ->
+            if (event != null) {
+                magneticFieldMeasurement = event.values
+                updateTextWithSensorValues()
+            }
+        } // for each sensor
+        state!!.sensorAccelerometerLiveData!!.observeForever { event: SensorLiveData.Event? ->
+            if (event != null) {
+                accelerometerMeasurement = event.values
+                updateTextWithSensorValues()
+            }
+        } // for each sensor
     }
 
     fun deregisterSensors() { // set all sensors as inactive
@@ -82,6 +96,8 @@ class AvoidOhioViewModel(application: Application) : AndroidViewModel(applicatio
             locationRequestsEnabled = false
             stopLocationUpdates(locationProviderClient,mLocationCallback)
         }
+        state!!.sensorMagneticFieldLiveData!!.setInactive() // required for each sensor you use
+        state!!.sensorAccelerometerLiveData!!.setInactive()
     }
 
     private fun registerSpecificSensor(sensorType: Int): SensorLiveData { // do not change
@@ -93,6 +109,8 @@ class AvoidOhioViewModel(application: Application) : AndroidViewModel(applicatio
     }
 
     private class SensorState { // add additional sensors here
-
+        lateinit var sensorGpsLiveData: Location
+        var sensorAccelerometerLiveData: SensorLiveData? = null
+        var sensorMagneticFieldLiveData: SensorLiveData? = null
     }
 }
