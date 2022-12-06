36bf6495a17be94b4ed4b32e13ff8db2b933ada6
Mitchell Bylak <mjbylak@uark.edu>
2022-12-05 23:56:15 -0600

    metalDetector-mitchell-finalized

--- app/src/main/java/com/example/android_swiss_army_knife/ui/speedometer/SpeedometerFragment.kt
+++ app/src/main/java/com/example/android_swiss_army_knife/ui/speedometer/SpeedometerFragment.kt
@@ -1,11 +1,22 @@
 package com.example.android_swiss_army_knife.ui.speedometer
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.android_swiss_army_knife.databinding.FragmentSpeedometerBinding
 
@@ -18,6 +29,8 @@ class SpeedometerFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val REQUEST_CODE_LOCATION_PERMISSION = 1
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,19 +38,81 @@ class SpeedometerFragment : Fragment() {
     ): View {
         speedometerViewModel = ViewModelProvider(this)[SpeedometerViewModel::class.java]
         // call register sensors
+        speedometerViewModel.registerSensors()
+
         _binding = FragmentSpeedometerBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textSpeedometer
-        speedometerViewModel.text.observe(viewLifecycleOwner) {
+        speedometerViewModel.speed.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        // call deregister sensors
         _binding = null
     }
+
+    override fun onResume() {
+        super.onResume()
+        speedometerViewModel.registerSensors()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        speedometerViewModel.unregisterSensors()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Bind the ViewModel to the fragment's lifecycle
+        speedometerViewModel.bind(this)
+
+        // Update the speedometer display with the current speed
+        speedometerViewModel.speed.observe(viewLifecycleOwner, Observer { speed ->
+            updateSpeedometer(speed)
+        })
+    }
+
+    private fun updateSpeedometer(speed: String) {
+        binding.textSpeedometer.text = speed
+    }
+
+    companion object {
+        private const val TAG = "SpeedometerFragment"
+    }
+
+    private fun requestPermissions() {
+        // Check if the permission has already been granted
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // If the permission has not been granted, request it
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION_PERMISSION
+            )
+        } else {
+            // If the permission has already been granted, register the sensors
+            speedometerViewModel.registerSensors()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        // Check if the permission was granted
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // If the permission was granted, register the sensors
+            speedometerViewModel.registerSensors()
+        }
+    }
+
 }
+
+
+
--- app/src/main/java/com/example/android_swiss_army_knife/ui/speedometer/SpeedometerViewModel.kt
+++ app/src/main/java/com/example/android_swiss_army_knife/ui/speedometer/SpeedometerViewModel.kt
@@ -1,14 +1,102 @@
 package com.example.android_swiss_army_knife.ui.speedometer
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.SensorManager
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.AndroidViewModel
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
 
-class SpeedometerViewModel(application: Application) : AndroidViewModel(application) {
+class SpeedometerViewModel(application: Application) : AndroidViewModel(application),
+    LocationListener {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is speedometer Fragment"
+    private val _speed = MutableLiveData<String>().apply {
+        value = "0.0"
     }
-    val text: LiveData<String> = _text
+    val speed: LiveData<String>
+        get() = _speed
+
+    private var isBound = false
+
+    fun bind(fragment: Fragment) {
+        if (!isBound) {
+            registerSensors()
+            isBound = true
+        }
+    }
+
+    fun unbind() {
+        if (isBound) {
+            unregisterSensors()
+            isBound = false
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private lateinit var context: Context
+
+    init {
+        context = application.applicationContext
+    }
+
+
+    private lateinit var sensorManager: SensorManager
+    private lateinit var locationManager: LocationManager
+
+    fun registerSensors() {
+        // Get the SensorManager and LocationManager instances
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Register a LocationListener to receive location updates
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            return
+        }
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0,
+            0f,
+            this
+        )
+    }
+
+    override fun onLocationChanged(location: Location) {
+        // Calculate the speed in meters per second
+        val speed = location.speed
+
+        // Convert the speed to kilometers per hour if needed
+        val speedKmh = speed * 3.6
+
+        // Update the speed LiveData with the current speed
+        _speed.value = speedKmh.toString()
+    }
+
+    fun unregisterSensors() {
+        // Unregister the LocationListener
+        locationManager.removeUpdates(this)
+    }
+
 }
